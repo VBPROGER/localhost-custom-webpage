71714a20c8f1da9ad833596b2fe2287c7cda5957
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-12-30 19:06:38 +0300

    Update localhost_hoster.py

--- src/localhost_hoster.py
+++ src/localhost_hoster.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-# Configuration
-FILE_NAME = 'index.html'
-
+# Configuration import: use yaml
+import yaml, os
 from flask import Flask, render_template
+
+DEF_CONF_NAME = 'web_conf.yaml'
+DEF_IP =        'localhost'
+DEF_PORT =      5000
+DEF_INDEX_F =   'index.html'
+try:
+    F =             open(DEF_CONF_NAME, 'r+')
+except (BaseException, Exception, IOError, OSError):
+    print('Please configure your server in file `web_conf.yaml`.')
+    os._exit(1)
+
+# Start
+config = yaml.safe_load(F.read())
 app = Flask(__name__)
+F.close() # Close file to avoid some problems..
 
 @app.route('/')
 def index():
-    return render_template(FILE_NAME)
+    # Index (main) page
+    return render_template(config.get('index') or DEF_INDEX_F)
 
 # Start server
-app.run(host = '0.0.0.0', port = 5000)
+app.run(host = config.get('ip') or DEF_IP, port = config.get('port') or DEF_PORT)
